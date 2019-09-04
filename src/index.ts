f37a7d9755a30864d88b6083fec1b03753990100
woorui <rui1009479218@gmail.com>
2019-09-05 00:18:11 +0800

    feat: complete -n feature

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,13 @@
-import xargs from './xargs';
-import fs from 'fs';
+import Xargs from './xargs';
+import parseProcessArgv from './parse_process_args';
 
-interface Iflag {
-	P?: number;
-	n?: number;
-	bin: string;
-}
-
-function parseProcessArgv(): Iflag {
-	const argvs = process.argv.slice(2);
-
-	let i = 0;
-	const obj = {};
-	while (i < argvs.length) {
-		if (argvs[i].includes('-')) {
-			if (i + 1 >= argvs.length) {
-				throw new Error('Error: flag must pair of key-value');
-			}
-			const [key, value] = [argvs[i].split('-')[1], argvs[i + 1]];
-			obj[key] = Number.isInteger(Number(value)) ? Number(value) : value;
-			i += 2;
-		} else {
-			obj['bin'] = argvs[i];
-			i++;
-		}
-	}
-
-	// Wash the flags object
-	const flags = Object.keys(obj).reduce((acc, key) => {
-		if (['P', 'n', 'bin'].includes(key)) {
-			acc[key] = obj[key];
-		}
-		return acc;
-	}, {} as Iflag);
-
-	return flags;
-}
 async function main() {
-	const { P: maxProcs = 0, n: maxArgs = 0, bin } = parseProcessArgv();
+	const flags = parseProcessArgv();
 
-	console.log(maxProcs, maxArgs, bin);
+	// Use interface, I can mock flag to test my Xargs class.
+	const xargs = new Xargs(flags);
+
+	await xargs.exec();
 }
 
-main();
+main(); 
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+export interface Iflag {
+	P?: number;
+	n?: number;
+	bin: string;
+}
